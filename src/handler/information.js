742a90a6460586006adee614415e573954e616a6
Jakob Hohlfeld <jhohlfeld@polyban.io>
2017-08-29 12:39:18 +0200

    Update information content.

--- src/handler/information.js
+++ src/handler/information.js
@@ -1,15 +1,14 @@
 import {
-  INFORMATION_MAIN_UTTERANCE,
   INTRO_PROMPT_UTTERANCE
 } from '../constants'
 import { wrapIn } from '../util'
 import { say } from '../alexa'
+import { information } from '../data.json'
 
 export default function informationHandler() {
-  const speechOutput = [
-    INFORMATION_MAIN_UTTERANCE,
-    INTRO_PROMPT_UTTERANCE
-  ].map(wrapIn('p')).join('')
+  const speechOutput = information.utterances
+    .concat(INTRO_PROMPT_UTTERANCE)
+    .map(wrapIn('p')).join('')
   const response = say(speechOutput)
     .reprompt(INTRO_PROMPT_UTTERANCE)
     .end(false)
